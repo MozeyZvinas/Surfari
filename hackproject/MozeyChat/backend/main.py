c5ccb9c0db23997792ca252ba6678b5d44ee5933
Moses Zvinavashe <morzeyzvinas@gmail.com>
2023-05-08 00:52:59 +0200

    Workspace Configuration

---
+++ hackproject/MozeyChat/backend/main.py
@@ -0,0 +1,29 @@
+# uvicorn main:app
+# uvicorn main:app --reload
+
+# Main imports
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+import openai
+
+# Custom function imports
+
+# Get Environment Vars
+
+# CORS - Origins
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:4173",
+    "http://localhost:3000",
+]
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    print("Hi Mozzillla")
+    return {"message": "Hello World"}
---
+++ hackproject/MozeyChat/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="">
+      <div className="text-2xl bg-blue-300">Hello</div>
+    </div>
+  );
+}
+
+export default App;
