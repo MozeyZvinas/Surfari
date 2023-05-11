497001c4bcd834e5c2959e84fd7bf3ccd158f6a3
Moses Zvinavashe <morzeyzvinas@gmail.com>
2023-05-11 21:29:36 +0200

    Updating

--- hackproject/MozeyChat/backend/functions/openai_requests.py
+++ hackproject/MozeyChat/backend/functions/openai_requests.py
@@ -12,28 +12,31 @@ openai.api_key = config("OPEN_AI_KEY")
 # Open AI - Whisper
 # Convert audio to text
 def convert_audio_to_text(audio_file):
-  try:
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    message_text = transcript["text"]
-    return message_text
-  except Exception as e:
-    return
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        message_text = transcript["text"]
+        return message_text
+    except Exception as e:
+        return
 
 # Open AI - Chat GPT
 # Convert audio to text
+
+
 def get_chat_response(message_input):
 
-  messages = get_recent_messages()
-  user_message = {"role": "user", "content": message_input + " Only say two or 3 words in Spanish if speaking in Spanish. The remaining words should be in English"}
-  messages.append(user_message)
-  print(messages)
+    messages = get_recent_messages()
+    user_message = {"role": "user", "content": message_input +
+                    " Speak in simple and plain English fo better under standing and Only translate when asked to"}
+    messages.append(user_message)
+    print(messages)
 
-  try:
-    response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=messages
-    )
-    message_text = response["choices"][0]["message"]["content"]
-    return message_text
-  except Exception as e:
-    return
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        message_text = response["choices"][0]["message"]["content"]
+        return message_text
+    except Exception as e:
+        return
